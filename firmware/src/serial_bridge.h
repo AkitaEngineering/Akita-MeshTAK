fd4b5d474c505e1db2536f9d25fa907cdd5ee0d9
sh4un <97253929+sh4un-dot-com@users.noreply.github.com>
2025-11-14 11:53:17 -0500

    Update serial_bridge.h

--- firmware/src/serial_bridge.h
+++ firmware/src/serial_bridge.h
@@ -1,11 +1,13 @@
-// firmware/src/serial_bridge.h
+// File: firmware/src/serial_bridge.h
+// Description: Declares Serial bridge functions for ATAK.
 #ifndef SERIAL_BRIDGE_H
 #define SERIAL_BRIDGE_H
 
 #ifdef ENABLE_SERIAL
+  #include <Arduino.h> // Include for size_t
   bool setupSerialBridge();
   void loopSerialBridge();
-  void sendDataSerial(const uint8_t* data, size_t len); // Add this
+  void sendDataSerial(const uint8_t* data, size_t len);
 #endif
 
 #endif
